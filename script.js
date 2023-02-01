e2c72f60d8e8e59fd23022f2a8d6cccd41411a4b
Danilopc <sr_danilo@hotmail.com>
2023-02-01 15:18:32 +0100

    lock previous days checkbox

--- script.js
+++ script.js
@@ -2,40 +2,47 @@ const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('.register-button');
 const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+const theDay = document.querySelector('.days')
 
-function addId(x){
+function addId(x) {
     x.setAttribute('id', today)
 }
 
+function blockDivs(){
+    const days = document.querySelectorAll('.day')
+    days.forEach(day => { day.style.pointerEvents = "none" })
+}
+
+function unlockLastDiv(){
+theDay.lastChild.style.pointerEvents= ""
+}
 
 button.addEventListener('click', add)
 form.addEventListener('change', save)
 
-
 function add() {
 
+  blockDivs()
+  
     dayExists = nlwSetup.dayExists(today)
-
+    
     if (dayExists) {
         alert("this day is already registered ⛔")
-        return
+       
     }
-   nlwSetup.addDay(today)
-   const theDay = document.querySelector('.days').lastElementChild
-   
-
-    addId(theDay)
-   
+        nlwSetup.addDay(today)
+        addId(theDay.lastElementChild)
+        unlockLastDiv()
 }
-
- 
-
+//blocked previous days, but stil unblocked after refresh
 
 function save() {
-    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+
+localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
 
 nlwSetup.setData(data);
-nlwSetup.load();
+nlwSetup.load(JSON.parse(localStorage.getItem('NLWSetup@habits')));
+
